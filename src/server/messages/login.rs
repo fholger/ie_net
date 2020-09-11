4e04949ddeec814ae54dea5cb7ac7423a616a5c6
Holger Frydrych <frydrych@mailbox.org>
2020-09-11 22:16:42 +0200

    Basic parsing of client login messages

--- src/server/messages/login.rs
+++ src/server/messages/login.rs
@@ -1,16 +1,21 @@
 use uuid::Uuid;
+use anyhow::{anyhow, Result};
+use byteorder::{LittleEndian, ReadBytesExt, BigEndian};
+use std::io::{Cursor, Read};
+use libflate::zlib;
+use crate::server::ClientStatus;
 
-struct IdentClientParams {
+pub struct IdentClientParams {
     game_version: Uuid,
     language: String,
 }
 
-struct LoginClientParams {
+pub struct LoginClientParams {
     username: String,
     password: String,
 }
 
-enum LoginClientMessage {
+pub enum LoginClientMessage {
     Ident(IdentClientParams),
     Login(LoginClientParams),
 }
@@ -30,4 +35,63 @@ enum LoginServerMessage {
     Welcome(WelcomeServerParams),
     Reject(RejectServerParams),
     Disconnect,
+}
+
+fn decompress_bytes(compressed_bytes: &[u8]) -> Result<Vec<u8>> {
+    let mut decoder = zlib::Decoder::new(compressed_bytes)?;
+    let mut decompressed = Vec::new();
+    decoder.read_to_end(&mut decompressed)?;
+    Ok(decompressed)
+}
+
+fn try_parse_string(reader: &mut impl ReadBytesExt) -> Result<String> {
+    let str_len = reader.read_u32::<LittleEndian>()?;
+    let mut str_data = vec![0u8; str_len as usize];
+    reader.read_exact(&mut str_data)?;
+    Ok(String::from_utf8(str_data)?)
+}
+
+impl LoginClientMessage {
+    pub fn try_parse(data: &mut Vec<u8>, client_status: ClientStatus) -> Result<Option<Self>> {
+        if data.len() < 4 {
+            return Ok(None);
+        }
+
+        let mut cursor = Cursor::new(&data);
+        let compressed_block_end = cursor.read_u32::<LittleEndian>()?;
+        if compressed_block_end > 4096 {
+            return Err(anyhow!("Suspiciously large block size, dropping client"));
+        }
+        if data.len() < compressed_block_end as usize {
+            return Ok(None);
+        }
+
+        let mut decompressed = decompress_bytes(&data[4..compressed_block_end as usize])?;
+        let message = match client_status {
+            ClientStatus::Connected => Self::Ident(Self::try_parse_ident(decompressed)?),
+            ClientStatus::Greeted => Self::Login(Self::try_parse_login(decompressed)?),
+            _ => return Err(anyhow!("Invalid client status")),
+        };
+        Ok(Some(message))
+    }
+
+    fn try_parse_ident(data: Vec<u8>) -> Result<IdentClientParams> {
+        let mut cursor = Cursor::new(data);
+        let game_version = Uuid::from_u128(cursor.read_u128::<BigEndian>()?);
+        let language = try_parse_string(&mut cursor)?;
+        Ok(IdentClientParams {
+            game_version,
+            language,
+        })
+    }
+
+    fn try_parse_login(data: Vec<u8>) -> Result<LoginClientParams> {
+        let mut cursor = Cursor::new(data);
+        let username = try_parse_string(&mut cursor)?;
+        let password = try_parse_string(&mut cursor)?;
+        Ok(LoginClientParams {
+            username,
+            password,
+        })
+    }
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -8,6 +8,7 @@ use async_std::{net::TcpListener, task};
 use futures::{AsyncReadExt, StreamExt};
 use std::collections::VecDeque;
 use uuid::Uuid;
+use messages::login::LoginClientMessage;
 
 pub fn run(addr: &str) -> Result<()> {
     let accept_task = accept_loop(addr);
@@ -22,33 +23,43 @@ async fn accept_loop(addr: &str) -> Result<()> {
     while let Some(connection) = incoming_connections.next().await {
         let connection = connection?;
         log::info!("New connection from {}", connection.peer_addr()?);
-        spawn_and_log_error(client_loop(connection));
+        spawn_and_log_error(client_login_handler(connection));
     }
 
     Ok(())
 }
 
-async fn client_loop(stream: TcpStream) -> Result<()> {
-    let client_addr = stream.peer_addr()?;
+#[derive(Clone, Copy)]
+pub enum ClientStatus {
+    Connected,
+    Greeted,
+    LoggedIn,
+}
+
+async fn client_login_handler(stream: TcpStream) -> Result<()> {
     let client_id = Uuid::new_v4();
+    let mut client_status = ClientStatus::Connected;
     let mut reader = BufReader::new(&stream);
     let mut read_buf = [0u8; 1024];
-    let mut received: VecDeque<u8> = VecDeque::with_capacity(1024);
+    let mut received = Vec::with_capacity(1024);
 
-    log::info!(
-        "Begin reading from client {} with generated client id {}",
-        client_addr,
-        client_id
-    );
+    log::info!("Starting login for new client with id {}", client_id);
 
     loop {
         let num_read = reader.read(&mut read_buf).await?;
         if num_read == 0 {
+            log::info!("Client {} closed the connection", client_id);
             break;
         }
-        received.extend(&read_buf[..num_read]);
+        received.extend_from_slice(&read_buf[..num_read]);
+
+        match LoginClientMessage::try_parse(&mut received, client_status)? {
+            Some(LoginClientMessage::Ident(params)) => continue,
+            Some(LoginClientMessage::Login(params)) => continue,
+            None => continue,
+        }
     }
-    log::info!("Client loop finished for {}", client_addr);
+    log::debug!("Client loop finished for client {}", client_id);
     Ok(())
 }
 
